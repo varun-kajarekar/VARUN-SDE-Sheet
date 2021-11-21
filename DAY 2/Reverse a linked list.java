23b335174e78d5b5e6f3fcbf03d48db33dd3dd40
varun-kajarekar <91301190+varun-kajarekar@users.noreply.github.com>
2021-11-21 12:24:52 +0530

    VARUN-SDE-Sheet

---
+++ DAY 2/Reverse a linked list.java
@@ -0,0 +1,14 @@
+//https://www.geeksforgeeks.org/reverse-a-linked-list/
+
+public static void reverse(){
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
