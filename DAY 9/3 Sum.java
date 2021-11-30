7009e950728d6501eaf17b101dcf56a074fa2633
varun-kajarekar <91301190+varun-kajarekar@users.noreply.github.com>
2021-11-30 19:14:17 +0530

    DAY 9

--- DAY 9/3 Sum.java
+++ DAY 9/3 Sum.java
@@ -30,3 +30,56 @@ public class Solution {
         return out;
     }
 }
+
+
+
+//                                    OR 
+
+
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(2);
+        arr.add(1);
+        arr.add(3);
+        arr.add(5);
+        arr.add(4);
+        Collections.sort(arr);
+        
+        int sum = 9;  //sum of three numbers in an array;
+        
+        
+        int n = arr.size();
+        int i = 1;
+        int j = n-1;
+        Boolean out = false;
+        for(int k=0;k<n-1;k++){
+            while(i<j){
+                int allsum = arr.get(k)+arr.get(i)+arr.get(j);  
+                if(allsum>sum){
+                    j--;
+                }
+                if(allsum==sum){
+                    out = true;
+                    System.out.println(arr.get(k)+" "+arr.get(i)+" "+arr.get(j)+" = "+allsum);
+                    break;
+                }
+                if(allsum<sum){
+                    i++;
+                }
+            }
+        }
+        System.out.println(out);
+	
+	}
+}
