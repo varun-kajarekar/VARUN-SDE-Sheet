5903b659198636bba1caf2d4bc9607bae2db3122
varun-kajarekar <91301190+varun-kajarekar@users.noreply.github.com>
2021-12-03 09:29:24 +0530

    Linked Lists

---
+++ DAY 11/Reverse Linked List.java
@@ -0,0 +1,27 @@
+// https://www.interviewbit.com/problems/reverse-linked-list/
+
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode A) {
+        ListNode prev = null;
+        ListNode current = A;
+        ListNode next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        A = prev;
+        return A;
+    }
+}
